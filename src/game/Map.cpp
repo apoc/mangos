ea3b1d9d2358a9235d54c9f48d122a3037deb39b
VladimirMangos <vladimir@getmangos.com>
2009-10-25 01:03:52 +0400

    [8725] Fixed bug with crash at forced logout in combat in instance.
    In fact this old bug that just has been triggred more often after recent code changes,
    so with some chance maybe some other logout related crashes fixed.
    
    Also small cleanup in avoid double calls RemoveFromWorld and CleanupsBeforeDelete in some cases.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -734,9 +734,10 @@ void Map::Remove(Player *player, bool remove)
     {
         if(remove)
             player->CleanupsBeforeDelete();
+        else
+            player->RemoveFromWorld();
 
         // invalid coordinates
-        player->RemoveFromWorld();
         player->ResetMap();
 
         if( remove )
@@ -759,8 +760,9 @@ void Map::Remove(Player *player, bool remove)
 
     if(remove)
         player->CleanupsBeforeDelete();
+    else
+        player->RemoveFromWorld();
 
-    player->RemoveFromWorld();
     RemoveFromGrid(player,grid,cell);
 
     SendRemoveTransports(player);
@@ -829,7 +831,11 @@ Map::Remove(T *obj, bool remove)
     if(obj->isActiveObject())
         RemoveFromActive(obj);
 
-    obj->RemoveFromWorld();
+    if(remove)
+        obj->CleanupsBeforeDelete();
+    else
+        obj->RemoveFromWorld();
+
     RemoveFromGrid(obj,grid,cell);
 
     UpdateObjectVisibility(obj,cell,p);
@@ -2160,9 +2166,6 @@ void Map::RemoveAllObjectsInRemoveList()
                 Remove((GameObject*)obj,true);
                 break;
             case TYPEID_UNIT:
-                // in case triggered sequence some spell can continue casting after prev CleanupsBeforeDelete call
-                // make sure that like sources auras/etc removed before destructor start
-                ((Creature*)obj)->CleanupsBeforeDelete ();
                 Remove((Creature*)obj,true);
                 break;
             default:
@@ -3486,8 +3489,6 @@ void Map::SendObjectUpdates()
     {
         Object* obj = *i_objectsToClientUpdate.begin();
         i_objectsToClientUpdate.erase(i_objectsToClientUpdate.begin());
-        if (!obj)
-            continue;
         obj->BuildUpdateData(update_players);
     }
 
