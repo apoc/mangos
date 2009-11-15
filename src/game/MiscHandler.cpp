6c8cdc97eb542474cd9a6dac777c7e3ae5b9aa25
balrok <balrok@getmangos.com>
2009-11-15 20:08:00 +0100

    [8821] removed some redundant player->SaveToDB calls
    SaveToDB will already be called at "SpawnCorpseBones"
    so no need to do this twice

--- src/game/MiscHandler.cpp
+++ src/game/MiscHandler.cpp
@@ -647,8 +647,6 @@ void WorldSession::HandleReclaimCorpseOpcode(WorldPacket &recv_data)
 
     // spawn bones
     GetPlayer()->SpawnCorpseBones();
-
-    GetPlayer()->SaveToDB();
 }
 
 void WorldSession::HandleResurrectResponseOpcode(WorldPacket & recv_data)
